336ba56c98f3259cca3175c9233aa9044707907d
Eduardo Pessine <eduardopessine@gmail.com>
2021-04-26 00:09:02 -0300

    Exercises 7.4-7.6

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -6,7 +6,7 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map(anecdote =>
-        <li>
+        <li key={anecdote.id}>
           <Link to={`/anecdotes/${anecdote.id}`}>
             {anecdote.content}
           </Link>
--- src/components/CreateNew.js
+++ src/components/CreateNew.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useField } from '../hooks/use-field';
 
 const CreateNew = ({ addNew, addNotification }) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
   const history = useHistory();
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
     history.push('/');
-    addNotification(`a new anecdote ${content} created!`);
+    addNotification(`a new anecdote ${content.value} created!`);
+  };
+  const resetAll = (e) => {
+    e.preventDefault();
+    content.reset();
+    author.reset();
+    info.reset();
   };
 
   return (
@@ -25,17 +32,18 @@ const CreateNew = ({ addNew, addNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content.inputFields} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author.inputFields} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info.inputFields} />
         </div>
         <button>create</button>
+        <button onClick={resetAll}>reset</button>
       </form>
     </div>
   );
